0676829031fd40378c6a9c3ffda0a47f6a271d9a
Geoffrey Challen <challen@buffalo.edu>
2015-09-24 16:04:48 -0400

    Tests working.

--- test/index.js
+++ test/index.js
@@ -12,6 +12,7 @@ var metalsmith = require('metalsmith'),
     updatedDefaults = require('../lib/updatedDefaults.js');
 
 chai.use(require('chai-fs'));
+chai.use(require('chai-datetime'));
 var assert = chai.assert;
 
 // 24 Sep 2015 : GWA : Dummy source directory for updated file.
@@ -48,14 +49,82 @@ describe('metalsmith-updated', function() {
         if (err) {
           return done(err);
         }
-        assert(files['new.html'].created == check_time);
-        assert(files['new.html'].updated == check_time);
+        assert.equalDate(files['new.html'].created, check_time);
+        assert.equalDate(files['new.html'].updated, check_time);
         assert(files['new.html'].modified == false);
-        assert(files['new.html'].created == files['new.html'].updated);
+        assert.equalDate(files['new.html'].created, files['new.html'].updated);
 
         assert.pathExists(test_defaults.updatedFile);
         var updated = jsonfile.readFileSync(test_defaults.updatedFile);
-        powerAssert.deepEqual(_.keys(updated).sort(), ['new.html']);
+        powerAssert.deepEqual(_.keys(updated).sort(), _.keys(files).sort());
+
+        check_files(files, defaults);
+        
+        done();
+      });
+  });
+  it('should ignore drafts with the default parameters', function(done) {
+    var defaults = _.clone(updatedDefaults.defaults);
+    var test_defaults = updatedDefaults.processConfig(defaults, path.join(src, 'src'));
+    reset_files(test_defaults);
+
+    var check_time = new Date();
+
+    metalsmith(src)
+      .use(function (files, metalsmith, innerDone) {
+        metalsmith.metadata()['date'] = check_time;
+        files['new.html'] = new Buffer("New contents");
+        files['ignore.html'] = new Buffer("Other contents");
+        files['ignore.html'].draft = true;
+        innerDone();
+      })
+      .use(updated(defaults))
+      .build(function (err, files) {
+        if (err) {
+          return done(err);
+        }
+        assert.equalDate(files['new.html'].created, check_time);
+        assert.equalDate(files['new.html'].updated, check_time);
+        assert(files['new.html'].modified == false);
+        assert.equalDate(files['new.html'].created, files['new.html'].updated);
+
+        assert.pathExists(test_defaults.updatedFile);
+        var updated = jsonfile.readFileSync(test_defaults.updatedFile);
+        powerAssert.deepEqual(_.keys(updated).sort(), ['new.html', '.gitignore'].sort());
+
+        check_files(files, defaults);
+        
+        done();
+      });
+  });
+  it('should include files based on patterns', function(done) {
+    var defaults = _.clone(updatedDefaults.defaults);
+    defaults.filePatterns = ["*.html"];
+    var test_defaults = updatedDefaults.processConfig(defaults, path.join(src, 'src'));
+    reset_files(test_defaults);
+
+    var check_time = new Date();
+
+    metalsmith(src)
+      .use(function (files, metalsmith, innerDone) {
+        metalsmith.metadata()['date'] = check_time;
+        files['new.html'] = new Buffer("New contents");
+        files['ignore.adoc'] = new Buffer("Other contents");
+        innerDone();
+      })
+      .use(updated(defaults))
+      .build(function (err, files) {
+        if (err) {
+          return done(err);
+        }
+        assert.equalDate(files['new.html'].created, check_time);
+        assert.equalDate(files['new.html'].updated, check_time);
+        assert(files['new.html'].modified == false);
+        assert.equalDate(files['new.html'].created, files['new.html'].updated);
+
+        assert.pathExists(test_defaults.updatedFile);
+        var updated = jsonfile.readFileSync(test_defaults.updatedFile);
+        powerAssert.deepEqual(_.keys(updated).sort(), ['new.html'].sort());
 
         check_files(files, defaults);
         
@@ -82,14 +151,14 @@ describe('metalsmith-updated', function() {
             if (err) {
               return done(err);
             }
-            assert(files['new.html'].created == check_time);
-            assert(files['new.html'].updated == check_time);
+            assert.equalDate(files['new.html'].created, check_time);
+            assert.equalDate(files['new.html'].updated, check_time);
             assert(files['new.html'].modified == false);
-            assert(files['new.html'].created == files['new.html'].updated);
+            assert.equalDate(files['new.html'].created, files['new.html'].updated);
 
             assert.pathExists(test_defaults.updatedFile);
             var updated = jsonfile.readFileSync(test_defaults.updatedFile);
-            powerAssert.deepEqual(_.keys(updated).sort(), ['new.html']);
+            powerAssert.deepEqual(_.keys(updated).sort(), _.keys(files).sort());
 
             check_files(files, defaults);
             
@@ -108,18 +177,97 @@ describe('metalsmith-updated', function() {
             if (err) {
               return done(err);
             }
-            console.log(files['new.html'].created, check_time);
-            assert(files['new.html'].created == check_time);
-            assert(files['new.html'].updated == check_time);
+            assert.equalDate(files['new.html'].created, check_time);
+            assert.equalDate(files['new.html'].updated, check_time);
             assert(files['new.html'].modified == false);
-            assert(files['new.html'].created == files['new.html'].updated);
+            assert.equalDate(files['new.html'].created, files['new.html'].updated);
 
             assert.pathExists(test_defaults.updatedFile);
             var updated = jsonfile.readFileSync(test_defaults.updatedFile);
-            powerAssert.deepEqual(_.keys(updated).sort(), ['new.html']);
+            powerAssert.deepEqual(_.keys(updated).sort(), _.keys(files).sort());
 
             check_files(files, defaults);
             
+            callback();
+          });
+      }],
+      function () {
+        done();
+      });
+  });
+  it('should not override explicit dates', function(done) {
+    var defaults = _.clone(updatedDefaults.defaults);
+    defaults.filePatterns = ["*.html"];
+    var test_defaults = updatedDefaults.processConfig(defaults, path.join(src, 'src'));
+    reset_files(test_defaults);
+
+    var check_time = new Date();
+    var created = new Date("2015-09-01 10:00:00 (EDT)");
+
+    async.series([
+      function (callback) {
+        metalsmith(src)
+          .use(function (files, metalsmith, innerDone) {
+            metalsmith.metadata()['date'] = check_time;
+            files['new.html'] = new Buffer("New contents");
+            files['manual.html'] = new Buffer("Other contents");
+            files['manual.html'].created = created;
+            innerDone();
+          })
+          .use(updated(defaults))
+          .build(function (err, files) {
+            if (err) {
+              return done(err);
+            }
+            assert.equalDate(files['new.html'].created, check_time);
+            assert.equalDate(files['new.html'].updated, check_time);
+            assert(files['new.html'].modified == false);
+            assert.equalDate(files['new.html'].created, files['new.html'].updated);
+
+            assert.equalDate(files['manual.html'].created, created)
+            assert.equalDate(files['manual.html'].updated, created)
+            assert(files['manual.html'].modified == false);
+            assert.equalDate(files['manual.html'].created, files['manual.html'].updated);
+
+            assert.pathExists(test_defaults.updatedFile);
+            var updated = jsonfile.readFileSync(test_defaults.updatedFile);
+            powerAssert.deepEqual(_.keys(updated).sort(), ['new.html', 'manual.html'].sort());
+
+            check_files(files, defaults);
+
+            callback();
+          });
+      },
+      function (callback) {
+        metalsmith(src)
+          .use(function (files, metalsmith, innerDone) {
+            metalsmith.metadata()['date'] = check_time;
+            files['new.html'] = new Buffer("New contents");
+            files['manual.html'] = new Buffer("Other contents");
+            files['manual.html'].created = created;
+            innerDone();
+          })
+          .use(updated(defaults))
+          .build(function (err, files) {
+            if (err) {
+              return done(err);
+            }
+            assert.equalDate(files['new.html'].created, check_time);
+            assert.equalDate(files['new.html'].updated, check_time);
+            assert(files['new.html'].modified == false);
+            assert.equalDate(files['new.html'].created, files['new.html'].updated);
+
+            assert.equalDate(files['manual.html'].created, created)
+            assert.equalDate(files['manual.html'].updated, created)
+            assert(files['manual.html'].modified == false);
+            assert.equalDate(files['manual.html'].created, files['manual.html'].updated);
+
+            assert.pathExists(test_defaults.updatedFile);
+            var updated = jsonfile.readFileSync(test_defaults.updatedFile);
+            powerAssert.deepEqual(_.keys(updated).sort(), ['new.html', 'manual.html'].sort());
+
+            check_files(files, defaults);
+
             callback();
           });
       }],
